39e6416852a72aec34ac184e3971575e70d3e695
jsilotoa <jsilotoa@uci.edu>
2023-07-16 23:09:48 -0700

    Annotate Dataset

--- download_weights.py
+++ download_weights.py
@@ -4,7 +4,9 @@ from pathlib import Path
 Path("./weights").mkdir(parents=True, exist_ok=True)
 
 model_urls = [
-    ("yolov5l_baseline.pt", 'https://drive.google.com/file/d/1TEKuVRz4PGfL4F34OcjFBdaruRjpV9Ev/view?usp=sharing')
+    ("yolov5l_baseline.pt", 'https://drive.google.com/file/d/1TEKuVRz4PGfL4F34OcjFBdaruRjpV9Ev/view?usp=sharing'),
+    ("yolov5s_daytime.pt", 'https://drive.google.com/file/d/1oaNW1-7gejJPDuHHrUwF1GlV8WQqse6U/view?usp=sharing'),
+    ("yolov5s_city_street.pt", 'https://drive.google.com/file/d/1OsspSWDWNFEzQGo48sLYdLyUtQZjDNzz/view?usp=sharing')
 ]
 
 for name, url in model_urls:
--- scripts/movie_dataset_annotate.sh
+++ scripts/movie_dataset_annotate.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 python download_weights.py
+python yolov5/val.py --data bdd100kvideo.yaml --weights weights/yolov5l_baseline.pt \
+ --save-txt --project movie_dataset --name val --exist-ok
 python yolov5/val.py --data bdd100kvideo_train.yaml --weights weights/yolov5l_baseline.pt \
- --save-txt --project movie_dataset --name val
-python yolov5/val.py --data bdd100kvideo_train.yaml --weights weights/yolov5l_baseline.pt \
- --save-txt --project movie_dataset --name train
+ --save-txt --project movie_dataset --name train --exist-ok
 
 mkdir -p /data/datasets/bdd100kvideo/labels/val
-mv yolov5/movie_dataset/val/labels/* /data/datasets/bdd100kvideo/labels
+cp movie_dataset/val/labels/* /data/datasets/bdd100kvideo/labels/val
 
 mkdir -p /data/datasets/bdd100kvideo/labels/train
-mv yolov5/movie_dataset/train/labels/* /data/datasets/bdd100kvideo/labels
+cp movie_dataset/train/labels/* /data/datasets/bdd100kvideo/labels/train
